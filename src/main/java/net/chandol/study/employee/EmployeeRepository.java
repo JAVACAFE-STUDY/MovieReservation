582b0d053ea3d1ad8d828acdfcec33bb1934c620
SejongPark <devsejong@gmail.com>
2016-06-30 00:00:52 +0900

    add React.js with sample code

---
+++ src/main/java/net/chandol/study/employee/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package net.chandol.study.employee;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+
+}
---
+++ src/main/java/net/chandol/study/employee/HomeController.java
@@ -0,0 +1,13 @@
+package net.chandol.study.employee;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value = "/")
+	public String index() {
+		return "index";
+	}
+}
