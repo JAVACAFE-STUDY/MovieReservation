ac99809302ebc93f24c79778ed890cb41d29a37a
SejongPark <devsejong@gmail.com>
2016-07-10 00:57:01 +0900

    문서 및 코드 정리

--- src/main/java/net/chandol/study/showing/ShowingCreator.java
+++ src/main/java/net/chandol/study/showing/ShowingCreator.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-public class ShowingCreatorService {
+public class ShowingCreator {
     @Autowired ShowingRepository showingRepository;
     @Autowired ShowingSeatRepository seatRepository;
 
