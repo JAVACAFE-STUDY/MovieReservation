6fdf5bfa463a1e29d220fc72cbf7bf88a053c92e
sjpark <g2sesfinkl@>
2016-07-12 15:30:44 +0900

    개선

--- src/main/java/net/chandol/study/showing/price/pricerule/DefaultPriceRule.java
+++ src/main/java/net/chandol/study/showing/price/pricerule/DefaultPriceRule.java
@@ -8,86 +8,110 @@ import net.chandol.study.theater.Theater;
 
 import java.time.DayOfWeek;
 import java.time.LocalDateTime;
-import java.util.AbstractMap.SimpleEntry;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Predicate;
 
 import static java.time.DayOfWeek.*;
 import static java.util.Arrays.stream;
-import static java.util.stream.Collectors.toMap;
 import static net.chandol.study.movie.MovieType.*;
-import static net.chandol.study.showing.price.pricerule.DefaultPriceRule.WeekTimePrice.Supporter.*;
+import static net.chandol.study.showing.price.pricerule.DefaultPriceRule.WeekTime.Supporter.isWeekDayAndHourBetween;
+import static net.chandol.study.showing.price.pricerule.DefaultPriceRule.WeekTime.Supporter.isWeekEndAndHourBetween;
+import static net.chandol.study.showing.price.pricerule.DefaultPriceRule.WeekTime.*;
 
 public class DefaultPriceRule implements PriceRule {
+
     @Override
     public Money evaluate(Money money, Theater theater, Movie movie, MovieType movieType, LocalDateTime showingTime) {
         return evaluate(showingTime, movieType);
     }
 
+    // 결과를 반환합니다.
     Money evaluate(LocalDateTime startTime, MovieType type) {
-        return WeekTimePrice.findPrice(type, startTime);
+        WeekTime weekTime = WeekTime.getWeekTime(startTime);
+        Integer price = priceMap.get(weekTime).get(type);
+
+        return Money.of(price);
+    }
+
+    // 가격정보가 설정됩니다.
+    private static final Map<WeekTime, Map<MovieType, Integer>> priceMap = new HashMap<>();
+    {
+        priceMap.put(WEEKDAY_MORNING, new HashMap<MovieType, Integer>() {{
+            put(_2D, 6000);
+            put(_3D, 8000);
+            put(_4DX, 10000);
+            put(IMAX, 10000);
+        }});
+
+        priceMap.put(WEEKDAY_NORMAL, new HashMap<MovieType, Integer>() {{
+            put(_2D, 8000);
+            put(_3D, 10000);
+            put(_4DX, 12000);
+            put(IMAX, 12000);
+        }});
+
+        priceMap.put(WEEKDAY_NIGHT, new HashMap<MovieType, Integer>() {{
+            put(_2D, 7000);
+            put(_3D, 9000);
+            put(_4DX, 11000);
+            put(IMAX, 11000);
+        }});
+
+        priceMap.put(WEEKEND_MORNING, new HashMap<MovieType, Integer>() {{
+            put(_2D, 7000);
+            put(_3D, 9000);
+            put(_4DX, 11000);
+            put(IMAX, 11000);
+        }});
+
+        priceMap.put(WEEKEND_NORMAL, new HashMap<MovieType, Integer>() {{
+            put(_2D, 9000);
+            put(_3D, 11000);
+            put(_4DX, 12000);
+            put(IMAX, 12000);
+        }});
+
+        priceMap.put(WEEKEND_PRIME, new HashMap<MovieType, Integer>() {{
+            put(_2D, 10000);
+            put(_3D, 12000);
+            put(_4DX, 14000);
+            put(IMAX, 14000);
+        }});
+
+        priceMap.put(WEEKEND_NIGHT, new HashMap<MovieType, Integer>() {{
+            put(_2D, 8000);
+            put(_3D, 10000);
+            put(_4DX, 11000);
+            put(IMAX, 11000);
+        }});
     }
 
 
-    enum WeekTimePrice {
-        WEEKDAY_MORNING(t -> isWeekDayAndHourBetween(t, 4, 10),
-                toPriceMap(
-                        entry(_2D, 6000), entry(_3D, 8000),
-                        entry(_4DX, 10000), entry(IMAX, 10000)
-                )),
-        WEEKDAY_NORMAL(
-                t -> isWeekDayAndHourBetween(t, 10, 22),
-                toPriceMap(
-                        entry(_2D, 8000), entry(_3D, 10000),
-                        entry(_4DX, 12000), entry(IMAX, 12000)
-                )),
-        WEEKDAY_NIGHT(
-                t -> isWeekDayAndHourBetween(t.minusHours(4), 22 - 4, 28 - 4),
-                toPriceMap(
-                        entry(_2D, 7000), entry(_3D, 9000),
-                        entry(_4DX, 11000), entry(IMAX, 11000)
-                )),
-        WEEKEND_MORNING(
-                t -> isWeekEndAndHourBetween(t, 4, 9),
-                toPriceMap(
-                        entry(_2D, 7000), entry(_3D, 9000),
-                        entry(_4DX, 11000), entry(IMAX, 11000)
-                )),
-        WEEKEND_NORMAL(
-                t -> isWeekEndAndHourBetween(t, 9, 17),
-                toPriceMap(
-                        entry(_2D, 9000), entry(_3D, 11000),
-                        entry(_4DX, 12000), entry(IMAX, 12000)
-                )),
-        WEEKEND_PRIME(
-                t -> isWeekEndAndHourBetween(t, 17, 23),
-                toPriceMap(
-                        entry(_2D, 10000), entry(_3D, 12000),
-                        entry(_4DX, 14000), entry(IMAX, 14000)
-                )),
-        WEEKEND_NIGHT(
-                t -> isWeekEndAndHourBetween(t.minusHours(4), 23 - 4, 28 - 4),
-                toPriceMap(
-                        entry(_2D, 8000), entry(_3D, 10000),
-                        entry(_4DX, 11000), entry(IMAX, 11000)
-                ));
+    enum WeekTime {
+        WEEKDAY_MORNING (t -> isWeekDayAndHourBetween(t, 4, 10)),
+
+        WEEKDAY_NORMAL  (t -> isWeekDayAndHourBetween(t, 10, 22)),
+
+        WEEKDAY_NIGHT   (t -> isWeekDayAndHourBetween(t.minusHours(4), 22 - 4, 28 - 4)),
+
+        WEEKEND_MORNING (t -> isWeekEndAndHourBetween(t, 4, 9)),
+
+        WEEKEND_NORMAL  (t -> isWeekEndAndHourBetween(t, 9, 17)),
+
+        WEEKEND_PRIME   (t -> isWeekEndAndHourBetween(t, 17, 23)),
+
+        WEEKEND_NIGHT   (t -> isWeekEndAndHourBetween(t.minusHours(4), 23 - 4, 28 - 4));
 
         private Predicate<LocalDateTime> predicator;
-        private Map<MovieType, Money> priceMap;
-        WeekTimePrice(Predicate<LocalDateTime> predicator, Map<MovieType, Money> priceMap) {
-            this.predicator = predicator;
-            this.priceMap = priceMap;
-        }
+        WeekTime(Predicate<LocalDateTime> predicator) {this.predicator = predicator;}
 
         //=====================================================================================================//
-        // WeekTime을 찾습니다.
-        public static Money findPrice(MovieType movieType, LocalDateTime dateTime) {
-            WeekTimePrice weekTimePrice = stream(values()).filter(x -> x.predicator.test(dateTime)).findFirst()
+        public static WeekTime getWeekTime(LocalDateTime dateTime) {
+            return stream(values()).filter(x -> x.predicator.test(dateTime)).findFirst()
                     .orElseThrow(() -> new IllegalArgumentException("구간을 찾지 못하였습니다. : " + dateTime));
-
-            return weekTimePrice.priceMap.get(movieType);
         }
 
         // WeekTime Helper
@@ -107,17 +131,8 @@ public class DefaultPriceRule implements PriceRule {
                 int hour = dateTime.getHour();
                 return hour >= startHour && hour < endHour;
             }
-
-            static Map<MovieType, Money> toPriceMap(Map.Entry<MovieType, Money>... entries) {
-                return stream(entries).collect(
-                        toMap(Map.Entry::getKey, Map.Entry::getValue)
-                );
-            }
-
-            static Map.Entry<MovieType, Money> entry(MovieType movieType, int price) {
-                return new SimpleEntry<>(movieType, Money.of(price));
-            }
         }
+
     }
 }
 
